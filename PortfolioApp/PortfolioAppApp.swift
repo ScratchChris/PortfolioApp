d5a7197fb48f3abfac455f21d1320f27644f1b61
Chris Turner <chris_turner@icloud.com>
2024-08-02 08:21:05 +0100

    Initial Commit

---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Chris Turner on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
